d619f12744aee8906d3f9f27cf55c52b474ce38d
Dennis Blommesteijn <dennis@blommesteijn.com>
2014-10-27 14:15:28 +0100

    Add java xml handers

---
+++ src/main/java/net/thepinguin/jp/AddRepository.java
@@ -0,0 +1,17 @@
+package net.thepinguin.jp;
+
+public class AddRepository {
+
+	public static void main(String[] args) {
+		try{
+			String pom_xml = args[0];
+			//TODO: read pom_xml into SAX
+			//TODO: test existance of repository
+			//TODO: append repository
+			//TODO: write changes to pom_xml
+		}catch(Exception e){
+			
+		}
+	}
+
+}
